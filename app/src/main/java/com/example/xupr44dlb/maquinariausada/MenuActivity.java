c4ae19739875ae63f85e37dc729cb21dab9d319c
Stefanie Lucero <slucero36@gmail.com>
2015-08-10 11:53:00 -0500

    Navigation drawer prev

--- app/src/main/java/com/example/xupr44dlb/maquinariausada/MenuActivity.java
+++ app/src/main/java/com/example/xupr44dlb/maquinariausada/MenuActivity.java
@@ -4,11 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 
+
 public class MenuActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
 
     }
 }
