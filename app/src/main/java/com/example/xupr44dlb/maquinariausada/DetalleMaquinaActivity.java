e4a9bdc7fa2b7f6628cbd84c1e024d2a1fd023f3
Stefanie Lucero <slucero36@gmail.com>
2015-08-28 23:34:27 -0500

    Gallery de imagenes

--- app/src/main/java/com/example/xupr44dlb/maquinariausada/DetalleMaquinaActivity.java
+++ app/src/main/java/com/example/xupr44dlb/maquinariausada/DetalleMaquinaActivity.java
@@ -1,20 +1,67 @@
 package com.example.xupr44dlb.maquinariausada;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Gallery;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class DetalleMaquinaActivity extends Activity {
-
+    static TextView mDotsText[];
+    private int mDotsCount;
+    private LinearLayout mDotsLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setUpWindow();
+        Gallery gallery = (Gallery)findViewById(R.id.gallery);
+        gallery.setAdapter(new ImageAdapter(this));
+        mDotsLayout = (LinearLayout)findViewById(R.id.image_count);
+        //here we count the number of images we have to know how many dots we need
+        mDotsCount = gallery.getAdapter().getCount();
 
+        //here we create the dots
+        //as you can see the dots are nothing but "."  of large size
+        mDotsText = new TextView[mDotsCount];
+
+        //here we set the dots
+        for (int i = 0; i < mDotsCount; i++) {
+            mDotsText[i] = new TextView(this);
+            mDotsText[i].setText(".");
+            mDotsText[i].setTextSize(45);
+            mDotsText[i].setTypeface(null, Typeface.BOLD);
+            mDotsText[i].setTextColor(android.graphics.Color.GRAY);
+            mDotsLayout.addView(mDotsText[i]);
+        }
+        //when we scroll the images we have to set the dot that corresponds to the image to White and the others
+        //will be Gray
+        gallery.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView adapterView, View view, int pos, long l) {
+
+                for (int i = 0; i < mDotsCount; i++) {
+                    DetalleMaquinaActivity.mDotsText[i]
+                            .setTextColor(Color.GRAY);
+                }
+
+                DetalleMaquinaActivity.mDotsText[pos]
+                        .setTextColor(Color.WHITE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView adapterView) {
+
+            }
+        });
     }
     public void setUpWindow() {
 
---
+++ app/src/main/java/com/example/xupr44dlb/maquinariausada/ImageAdapter.java
@@ -0,0 +1,49 @@
+package com.example.xupr44dlb.maquinariausada;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    //array of integers for images IDs
+    private Integer[] mImageIds = {
+            R.drawable.maquina,
+            R.drawable.ic_iiasa,
+            R.drawable.maquina
+
+    };
+
+    //constructor
+    public ImageAdapter (Context c){
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ImageView imageView = new ImageView(mContext);
+
+        imageView.setImageResource(mImageIds[i]);
+        imageView.setLayoutParams(new Gallery.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+        return imageView;
+    }
+}
